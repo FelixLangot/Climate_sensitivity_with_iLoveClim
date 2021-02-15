1b9f4411f74c674936548282024515da90a12190
Felix Langot <felixlangot@gmail.com>
2021-02-15 16:59:15 +0100

    Final version with statistics

--- Script_CTRL.py
+++ Script_CTRL.py
@@ -29,8 +29,8 @@ def spatialmean(V,lat):		# lat: vecteur 1D des lattitudes
 
 
 # Dark graph
-plt.style.use('dark_background')
-#plt.style.use('default')
+#plt.style.use('dark_background')
+plt.style.use('default')
 
 ##### Récupération et observation des variables des fichiers netcdf #####
 # Récupération des données du fichier fichier.nc dans la structure data
@@ -161,7 +161,7 @@ plt.show()
 #%%
 
 #%%
-Tdiff = TsevolC - np.mean(Tsevol[400:500])
+Tdiff = TsevolC - np.mean(Tsevol[900:1000])
 plt.plot(t2, Tdiff, linewidth=1)
 plt.title('Temperature difference', fontsize=20)					# Titre
 plt.xlabel('$t$ (yr)', fontsize=20)			# Axe des abscisses
@@ -250,7 +250,7 @@ print('#######################################################################')
 TmCTRL = np.zeros((32,64))
 for i in np.arange(0, len(lat)):
     for j in np.arange(0,len(lon)):
-        TmCTRL[i,j] = np.mean(Ts[0:500,i,j])
+        TmCTRL[i,j] = np.mean(Ts[400:500,i,j])
 
 map = Basemap(projection='merc', llcrnrlon=-180., llcrnrlat=-80.,
               urcrnrlon=180., urcrnrlat=80., resolution='c')
@@ -282,13 +282,13 @@ plt.show()
 
 #%%
 print('#######################################################################')
-print('Difference temperature moyenne sur 500 ans 2D')
+print('Difference temperature moyenne sur 100 derniers ans 2D')
 print('#######################################################################')
 
 Tm1pcCO2 = np.zeros((32, 64))
 for i in np.arange(0, len(lat)):
     for j in np.arange(0, len(lon)):
-        Tm1pcCO2[i, j] = np.mean(Ts1pcCO2[0:500, i, j])
+        Tm1pcCO2[i, j] = np.mean(Ts1pcCO2[400:500, i, j])
 
 Tdiff2 = Tm1pcCO2 - TmCTRL
 
@@ -299,7 +299,7 @@ lons, lats = np.meshgrid(lon-180, lat)
 
 x, y = map(lons, lats)
 # Carte de la variable
-temp = map.pcolormesh(x, y, Tdiff2, cmap='coolwarm')
+temp = map.pcolormesh(x, y, Tdiff2, cmap='Reds')
 map.drawcoastlines() 			# lignes de côte
 paralleles = [-60, -30, 0, 30, 60]
 meridiens = [-120, -60, 0, 60, 120]
@@ -309,40 +309,55 @@ map.drawparallels(paralleles, labels=[
 # labels = [left,right,top,bottom]
 map.drawmeridians(meridiens, labels=[
     False, False, False, True], fontsize=10)
-cmap = 'coolwarm'
+cmap = 'Reds'
 colormesh = map.pcolormesh(x, y, Tdiff2, cmap=cmap)
 cb = map.colorbar(colormesh)
 #    cb = map.colorbar(temp,"right", size="5%", pad="2%", vmin=220, vmax=310)
 cb.set_label('$T$ (K)', fontsize=20)
-plt.title('Diff', fontsize=20)
+#plt.title('Diff', fontsize=20)
 # Sauvegarde
 plt.savefig("../Graph2D/Diff/diff500.eps", format='EPS', dpi=600)
 plt.show()
 #%%
 
 #%%
-######################## Questions TP #################################
+print('######################## Questions TP #################################')
 
 print(data.variables['tsr'])
 print(data.variables['ttr'])
+print(data2.variables['time'])
 
 time = data2.variables['time'][:]
 
 TsrCTRL = data.variables['tsr'][:]
 TtrCTRL = data.variables['ttr'][:]
 
-Tsr1pcCO2 = data2.variables['tsr'][:]
-Ttr1pcCO2 = data2.variables['ttr'][:]
+Tsr1pcCO2 = data2.variables['tsr'][1:500]
+Ttr1pcCO2 = data2.variables['ttr'][1:500]
+
+Tsrm = spatialmean(Tsr1pcCO2, lat)
+Ttrm = spatialmean(Ttr1pcCO2, lat)
+
+t3 = np.arange(len(Tsrm))
+
+plt.plot(t3, Tsrm)
+plt.plot(t3, Ttrm)
+plt.show()
 
 NtCTRL = TsrCTRL - TtrCTRL
-Nt1pcCO2 = Ts1pcCO2 - Ttr1pcCO2
+Nt1pcCO2 = Tsr1pcCO2 - Ttr1pcCO2
 
 NtmCTRL = spatialmean(NtCTRL, lat)
-Ntm1pcCO2 = spatialmean(Nt1pcCO2, lat)
+Ntm1pcCO2i = spatialmean(Nt1pcCO2, lat)
+
+Ntm1pcCO2 = Tsrm-Ttrm
 
 ΔN = Ntm1pcCO2 - np.mean(NtmCTRL[900:1000])
+ΔNi = Ntm1pcCO2i - np.mean(NtmCTRL[900:1000])
 
-plt.plot(t2, ΔN, linewidth=1)
+plt.plot(t3, ΔN, linewidth=1)
+plt.show()
+plt.plot(t3, ΔNi, linewidth=1)
 #plt.title('Delta N', fontsize=20)					# Titre
 plt.xlabel('t (yr)', fontsize=20)			# Axe des abscisses
 plt.ylabel('$\Delta N (W \cdot m^{-2}$)', fontsize=20)			        # Axe des ordonnées
@@ -356,15 +371,16 @@ plt.show()
 #%%
 # Calculer le(s) changement(s) de pente
 
+Tdiff = TsevolC - np.mean(Tsevol[900:1000])
+Tdiff = np.delete(Tdiff, 0)
+
 aa,bb,cc,dd,ee = np.polyfit(Tdiff, ΔN, 4)                          # On construit un polynome de degre 4 pour qu'il suive toutes les données
 ΔNfit = aa*Tdiff**4 + bb*Tdiff**3 + cc*Tdiff**2 + dd*Tdiff + ee    # On construit le fit a partir des coefficients du polynome 
 plt.scatter(Tdiff, ΔN, linewidth=1)                                # graph des données
 plt.plot(Tdiff, ΔNfit, color='r')                                  # graph du fit
 plt.show()
-dΔN = 4*aa*Tdiff**3 + 3*bb*Tdiff**3 + 2*cc*Tdiff + dd              # On calcule la dérivée du fit pour identifier les changements de pente
-plt.scatter(Tdiff, dΔN, color='r')                                 # graph de la dérivée du fit
-plt.plot( Tdiff, np.zeros((len(Tdiff))) )                          # On trace une ligne en dΔN = 0
-plt.show()
+dΔN = 4*aa*Tdiff**3 + 3*bb*Tdiff**2 + 2*cc*Tdiff + dd              # On calcule la dérivée du fit pour identifier les changements de pente
+
 ilist = []
 for i in np.arange(0,len(Tdiff)):                                  # On identifie quels éléments de dΔN sont supérieurs à 0 et on met leur indice dans une liste
     o = dΔN[i]
@@ -373,6 +389,14 @@ for i in np.arange(0,len(Tdiff)):                                  # On identifi
 ilist = np.asarray(ilist)
 midstart = np.min(ilist)                                           # On définit midstart et midstop comme étant les indices correspondant à l'endroit ou la pente est positive (au milieu du graph 1pcCO2)
 midstop = np.max(ilist)
+
+# graph de la dérivée du fit
+plt.scatter(Tdiff, dΔN, color='r')
+plt.scatter(Tdiff[midstart:midstop], dΔN[midstart:midstop], color='b')
+# On trace une ligne en dΔN = 0
+plt.plot(Tdiff, np.zeros((len(Tdiff))))
+plt.show()
+
 print(midstart, midstop)
 
 #%%
@@ -380,40 +404,43 @@ print(midstart, midstop)
 #%%
 # λc, Fc, ΔTc
 
-a,b = np.polyfit(Tdiff[0:midstart], ΔN[0:midstart], 1)                             # On construit les fits séparés (des droites) avec les indices correspondant qu'on trouve avec la methode d'au-dessus     
-c,d = np.polyfit(Tdiff[midstart:midstop], ΔN[midstart:midstop], 1)
-e,f = np.polyfit(Tdiff[midstop:500], ΔN[midstop:500], 1)
+#a,b = np.polyfit(Tdiff[0:midstart], ΔN[0:midstart], 1)                             # On construit les fits séparés (des droites) avec les indices correspondant qu'on trouve avec la methode d'au-dessus     
+#c,d = np.polyfit(Tdiff[midstart:midstop], ΔN[midstart:midstop], 1)
+#e,f = np.polyfit(Tdiff[midstop:500], ΔN[midstop:500], 1)
 
 ovrslope, ovrintercept = np.polyfit(Tdiff, ΔN, 1)
 
-plt.plot(Tdiff[0:20], a*Tdiff[0:20] + b, linewidth=4, color='r')                   # On trace les données avec les fits
-plt.plot(Tdiff[20:100], c*Tdiff[20:100] + d, linewidth=4, color='g')
-plt.plot(Tdiff[100:500], e*Tdiff[100:500] + f, linewidth=4, color='y')
-plt.plot(Tdiff, ovrslope*Tdiff + ovrintercept, linewidth=4, color='b')
-plt.scatter(Tdiff, ΔN, c=time, marker='o')
+#plt.plot(Tdiff[0:20], a*Tdiff[0:20] + b, linewidth=2, color='r')                   # On trace les données avec les fits
+#plt.plot(Tdiff[20:100], c*Tdiff[20:100] + d, linewidth=2, color='g')
+#plt.plot(Tdiff[100:500], e*Tdiff[100:500] + f, linewidth=2, color='y')
+plt.plot(Tdiff, ovrslope*Tdiff + ovrintercept, linewidth=2, color='g')
+plt.scatter(Tdiff, ΔN, c=t3, marker='o')
 #plt.title('DeltaN vs DeltaT', fontsize=20)	
 plt.xlabel('$\Delta T$ (K)', fontsize=20)				
-plt.ylabel('$\Delta N ~(W \cdot m^{-2})$', fontsize=20)				        
+plt.ylabel('$N ~(W \cdot m^{-2})$', fontsize=20)				        
 #legend = plt.legend(loc='upper center', shadow=True)
-plt.viridis()
+#plt.viridis()
+plt.plasma()
 plt.savefig('../Graph1D/DeltaNvsDeltaT.eps', format='eps', dpi=600)
 plt.show()
 
 ΔTc = ovrintercept/(-ovrslope)
 
-print("RED")
-print("-slope = λ1 = ", -a)
-print("intercept = F1 = ", b)
+#print("RED")
+#print("-slope = λ1 = ", -a)
+#print("intercept = F1 = ", b)
+#print("GREEN")
+#print("-slope = λ2 = ", -c)
+#print("intercept = F2 = ", d)
+#print("YELLOW")
+#print("-slope = λ3 = ", -e)
+#print("intercept = F3 = ", f)
 print("GREEN")
-print("-slope = λ2 = ", -c)
-print("intercept = F2 = ", d)
-print("YELLOW")
-print("-slope = λ3 = ", -e)
-print("intercept = F3 = ", f)
-print("BLUE")
 print('-overall slope = λc = ', -ovrslope)
 print('overall intecept = Fc = ', ovrintercept)
-print('change in temperature at equilibrium (mean over 500 yrs) = ΔTc = ', ΔTc)
+print('change in temperature at equilibrium = ΔTc = ', ΔTc)
+print("Pearson's correlation coefficient ρ = ",
+      np.corrcoef(Tdiff, ΔN)[0, 1])
 
 #%%
 
@@ -422,19 +449,20 @@ print('change in temperature at equilibrium (mean over 500 yrs) = ΔTc = ', ΔTc
 
 Tdiffeq = Tdiff[400:500]
 ΔNeq = ΔN[400:500]
-time100 = time[400:500]
+time100 = time[401:500]
 
 slopeeq, intercepteq = np.polyfit(Tdiffeq, ΔNeq, 1)
 
 # On trace les données avec les fits
 
-plt.plot(Tdiffeq, slopeeq*Tdiffeq + intercepteq, linewidth=4, color='r')
+plt.plot(Tdiffeq, slopeeq*Tdiffeq + intercepteq, linewidth=2, color='g')
 plt.scatter(Tdiffeq, ΔNeq, c=time100, marker='o')
 #plt.title('DeltaN vs DeltaT', fontsize=20)
 plt.xlabel('$\Delta T$ (K)', fontsize=20)
-plt.ylabel('$\Delta N ~(W \cdot m^{-2})$', fontsize=20)
+plt.ylabel('$N ~(W \cdot m^{-2})$', fontsize=20)
 #legend = plt.legend(loc='upper center', shadow=True)
-plt.viridis()
+#plt.viridis()
+plt.plasma()
 plt.savefig('../Graph1D/DeltaNvsDeltaTeq.eps', format='eps', dpi=600)
 plt.show()
 
@@ -443,7 +471,7 @@ plt.show()
 print('λeq = ', -slopeeq)
 print('Feq = ', intercepteq)
 print('ΔTeq  = ', ΔTeq)
-
+print("Pearson's correlation coefficient ρ = ", np.corrcoef(Tdiffeq, ΔNeq)[0,1])
 
 #%%
 
@@ -458,13 +486,14 @@ sloper, interceptr = np.polyfit(Tdiffr, ΔNr, 1)
 
 # On trace les données avec les fits
 
-plt.plot(Tdiffr, sloper*Tdiffr + interceptr, linewidth=4, color='r')
+plt.plot(Tdiffr, sloper*Tdiffr + interceptr, linewidth=2, color='g')
 plt.scatter(Tdiffr, ΔNr, c=time100, marker='o')
 #plt.title('DeltaN vs DeltaT', fontsize=20)
 plt.xlabel('$\Delta T$ (K)', fontsize=20)
-plt.ylabel('$\Delta N ~(W \cdot m^{-2})$', fontsize=20)
+plt.ylabel('$N ~(W \cdot m^{-2})$', fontsize=20)
 #legend = plt.legend(loc='upper center', shadow=True)
-plt.viridis()
+#plt.viridis()
+plt.plasma()
 plt.savefig('../Graph1D/DeltaNvsDeltaTr.eps', format='eps', dpi=600)
 plt.show()
 
@@ -473,5 +502,15 @@ plt.show()
 print('λr = ', -sloper)
 print('Fr = ', interceptr)
 print('ΔTr  = ', ΔTr)
+print("Pearson's correlation coefficient ρ = ",
+      np.corrcoef(Tdiffr, ΔNr)[0, 1])
+
+Templist = [ΔTc, ΔTeq, ΔTr]
+Flist = [ovrintercept, intercepteq, interceptr]
+lambdalist = [-ovrslope, -slopeeq, -sloper]
+
+print('T', Templist)
+print('F', Flist)
+print('λ', lambdalist)
 
 # %%
